d63221df82ec0bc065bf69384081c3ffa50e4df9
Siitrev <paplata50@gmail.com>
2024-01-05 21:39:13 +0100

    Add template classes

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-if __name__ == "__main__":
-    print(__name__)
+from core.template.template import *
+import benchmark_functions as bf
+
+
+def fun(x):
+    return 2*np.power(x,2) + 5
+fitness_function = bf.Michalewicz()
+
+info = ChromosomeInfo(-10,10,6)
+pop = Population(200,info)
+[print(x) for x in pop.people]
+print(fitness_function([2.202906,1.570796]))
