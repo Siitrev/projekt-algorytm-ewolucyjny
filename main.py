b0abaa8f687e97a4d9e8e452f426253d0ff0c8bc
Siitrev <paplata50@gmail.com>
2024-01-06 16:32:54 +0100

    "Template.py" format

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from core.template.template import *
 import benchmark_functions as bf
-import pprint as pp 
+import pprint as pp
 
 
 def fun(x):
-    return 2*np.power(x,2) + 5
+    return 2 * np.power(x, 2) + 5
+
+
 fitness_function = bf.Michalewicz()
 
-info = ChromosomeInfo(-10,10,6)
-pop = Population(10,info)
+info = ChromosomeInfo(-10, 10, 6)
+pop = Population(10, info)
 print(pop)
 for index, person in enumerate(pop.people):
-    person.chromosome.set("0011111100101010101010001") 
+    person.chromosome.set("0011111100101010101010001")
 print(pop)
 # mut = Mutation(n = 1)
-# Experiment.mutate(mut)      
+# Experiment.mutate(mut)
 # for i in range(20):
 #     Experiment.population.set_best_people()
 #     Experiment.cross()
