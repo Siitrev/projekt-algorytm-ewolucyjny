d3b4532a894dadfe98c2562cd6408b31049c5ae2
AMDevRadeon <korseka@protonmail.com>
2024-01-06 20:06:07 +0100

    added inversion function

--- core/inversion/inversion.py
+++ core/inversion/inversion.py
@@ -1,1 +1,16 @@
 # Rdz 1, 3
+import numpy as np
+
+
+def inversion(chromosome : str) -> str:
+    invert = {'0':'1', '1':'0'}
+    inversion_points = np.random.randint(low=0, high=len(chromosome)+1, size=2)
+    print(inversion_points)
+
+    anaphase = [*chromosome]
+    for i in range(*sorted(inversion_points)):
+        anaphase[i] = invert[anaphase[i]]
+
+    return ''.join(anaphase)
+
+print(inversion("00110101010"))
