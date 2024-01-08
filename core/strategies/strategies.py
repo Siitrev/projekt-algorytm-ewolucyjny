b8dd23a87c9f729e2848aa51f9bef7992cff1e01
Siitrev <paplata50@gmail.com>
2024-01-08 10:03:37 +0100

    Format strategies.py

--- core/strategies/strategies.py
+++ core/strategies/strategies.py
@@ -6,9 +6,9 @@ class Strategy:
     def best_selection(
         self, population: Population, amount: int, maximization=False
     ) -> list[Person]:
-        sample = sorted(population.people, reverse=maximization, key=lambda person: person.value)[
-            :amount
-        ]
+        sample = sorted(
+            population.people, reverse=maximization, key=lambda person: person.value
+        )[:amount]
         return sample
 
     def tournament_selection(
@@ -20,7 +20,11 @@ class Strategy:
     ) -> list[Person]:
         sample = []
         for _ in range(tournaments):
-            tournament = list(np.random.choice(population.people, amount_of_contestants, replace=False))
+            tournament = list(
+                np.random.choice(
+                    population.people, amount_of_contestants, replace=False
+                )
+            )
             print(tournament)
             if maximization:
                 best_contestant = max(tournament, key=lambda person: person.value)
@@ -29,19 +33,31 @@ class Strategy:
             sample.append(best_contestant)
         return sample
 
-    def roulette_wheel(self, population: Population, amount : int, maximization=False) -> list[Person]:
+    def roulette_wheel(
+        self, population: Population, amount: int, maximization=False
+    ) -> list[Person]:
         if maximization:
             minimum_person = min(population.people, key=lambda person: person.value)
             minimum_value = np.fabs(minimum_person.value)
-            sum_of_fitness = sum([person.value + minimum_value + 0.1 for person in population.people])
+            sum_of_fitness = sum(
+                [person.value + minimum_value + 0.1 for person in population.people]
+            )
         else:
-            minimum_person = min(population.people, key=lambda person: 1/person.value)
-            minimum_value = np.fabs(1/minimum_person.value)
-            sum_of_fitness = sum([1/person.value + minimum_value + 0.1 for person in population.people])
-        probability = tuple(enumerate([(1/person.value+minimum_value + 0.1)/sum_of_fitness for person in population.people]))
+            minimum_person = min(population.people, key=lambda person: 1 / person.value)
+            minimum_value = np.fabs(1 / minimum_person.value)
+            sum_of_fitness = sum(
+                [1 / person.value + minimum_value + 0.1 for person in population.people]
+            )
+        probability = tuple(
+            enumerate(
+                [
+                    (1 / person.value + minimum_value + 0.1) / sum_of_fitness
+                    for person in population.people
+                ]
+            )
+        )
         sample = []
         for _ in range(amount):
-            
             wheel_result = np.random.rand()
             distribution = 0
             for ind, prob in probability:
@@ -49,6 +65,5 @@ class Strategy:
                     sample.append(population.people[ind])
                     break
                 distribution += prob
-                
+
         return sample
-        
