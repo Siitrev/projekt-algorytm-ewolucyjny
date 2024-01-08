3c3d4b35371bd825f3120f000725efbefbe4e156
Siitrev <paplata50@gmail.com>
2024-01-08 10:11:37 +0100

    Format mutation.py

--- core/mutations/mutation.py
+++ core/mutations/mutation.py
@@ -1,20 +1,21 @@
 import numpy as np
 from core.template.template import Chromosome
 
-def mutation(old_chromosome : Chromosome, points : int) -> str:
+
+def mutation(old_chromosome: Chromosome, points: int) -> str:
     genome = old_chromosome.get()  # bo get zwraca self.genome ?
     length = len(genome)
     inverted = list(genome)  # bo na stringu sie nie da
 
     # Losowanie indeksu bitu
     index = np.random.randint(0, length)
-    inverted[index] = '0' if genome[index] == '1' else '1'
+    inverted[index] = "0" if genome[index] == "1" else "1"
 
     if points == 2:
         index2 = np.random.randint(0, length)
-        while index2==index:
+        while index2 == index:
             index2 = np.random.randint(0, length)
-        inverted[index2] = '0' if genome[index2] == '1' else '1'
+        inverted[index2] = "0" if genome[index2] == "1" else "1"
 
     # new_chromosome = inverted
     new_chromosome = "".join(inverted)
